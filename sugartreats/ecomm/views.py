c94341c287aca8a68f9140412f0224c5263e1948
Maisha <maisha.sup@gmail.com>
2023-08-21 12:37:42 -0700

    Add some products to Products page using queries

--- sugartreats/ecomm/views.py
+++ sugartreats/ecomm/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import*
 
 # Create your views here.
 def home(request):
     return render(request,"ecomm/dashboard.html")
 
 def products(request):
-    return render(request,"ecomm/products.html")
+    products = Product.objects.all()
+    return render(request,"ecomm/products.html", {"products": products})
 
 def customers(request):
     return render(request,"ecomm/customers.html")
