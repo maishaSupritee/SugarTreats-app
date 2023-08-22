ccf4e745978b24ac12d7a18be617a8f17bb890f9
Maisha <maisha.sup@gmail.com>
2023-08-21 21:11:18 -0700

    Fill in the dashboard with values

--- sugartreats/ecomm/views.py
+++ sugartreats/ecomm/views.py
@@ -4,7 +4,25 @@ from .models import*
 
 # Create your views here.
 def home(request):
-    return render(request,"ecomm/dashboard.html")
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    products = Product.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status = "Delivered").count()
+    fulfilled = orders.filter(status = "Fulfilled").count()
+    pending = orders.filter(status = "Pending").count()
+    context = {
+        "orders": orders, 
+        "customers": customers, 
+        "products": products,
+        "total_customers": total_customers,
+        "total_orders": total_orders,
+        "delivered": delivered,
+        "pending": pending,
+        "fulfilled": fulfilled
+        }
+    return render(request,"ecomm/dashboard.html", context)
 
 def products(request):
     products = Product.objects.all()
